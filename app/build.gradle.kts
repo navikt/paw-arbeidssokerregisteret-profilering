e314ac959ada484d1315812f718ab7abec7c5049
Nils Martin Sande <nils.martin.sande@nav.no>
2024-01-09 15:12:25 +0100

    La til initiell kafka config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.github.davidmc24.gradle.plugin.avro.GenerateAvroProtocolTask
+
 plugins {
     kotlin("jvm") version "1.9.20"
     id("io.ktor.plugin") version "2.3.5"
+    id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
     application
 }
 
@@ -9,18 +12,34 @@ repositories {
     maven {
         url = uri("https://packages.confluent.io/maven/")
     }
+    mavenNav("paw-arbeidssokerregisteret")
 }
 
+val arbeidssokerregisteretVersion = "23.12.18.110-1"
 val navCommonModulesVersion = "2.2023.01.02_13.51-1c6adeb1653b"
 val logstashVersion = "7.3"
-val logbackVersion = "1.4.5"
+val logbackVersion = "1.4.12"
+val pawUtilsVersion = "23.12.20.5-1"
+
+val schema by configurations.creating {
+    isTransitive = false
+}
 
 dependencies {
+    schema("no.nav.paw.arbeidssokerregisteret.api.schema:eksternt-api:$arbeidssokerregisteretVersion")
     implementation(pawObservability.bundles.ktorNettyOpentelemetryMicrometerPrometheus)
+    implementation("no.nav.paw.hoplite-config:hoplite-config:$pawUtilsVersion")
+
+    //logging
     implementation("no.nav.common:log:$navCommonModulesVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
-    implementation("io.confluent:kafka-avro-serializer:7.4.0")
+
+    //Kafka
+    implementation("no.nav.paw.kafka:kafka:$pawUtilsVersion")
+    implementation("io.confluent:kafka-avro-serializer:7.5.3")
+    implementation("org.apache.avro:avro:1.11.3")
+    implementation("org.apache.kafka:kafka-clients:3.6.0")
 
     // Use the Kotlin JUnit 5 integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
@@ -30,6 +49,10 @@ dependencies {
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
+}
+
+tasks.named("generateAvroProtocol", GenerateAvroProtocolTask::class.java) {
+    source(zipTree(schema.singleFile))
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -59,5 +82,17 @@ tasks.withType(Jar::class) {
         attributes["Implementation-Version"] = project.version
         attributes["Implementation-Title"] = rootProject.name
         attributes["Main-Class"] = application.mainClass.get()
+    }
+}
+
+fun RepositoryHandler.mavenNav(repo: String): MavenArtifactRepository {
+    val githubPassword: String by project
+
+    return maven {
+        setUrl("https://maven.pkg.github.com/navikt/$repo")
+        credentials {
+            username = "x-access-token"
+            password = githubPassword
+        }
     }
 }
--- app/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/profilering/App.kt
+++ app/src/main/kotlin/no/nav/paw/arbeidssokerregisteret/profilering/App.kt
@@ -1,8 +1,17 @@
 package no.nav.paw.arbeidssokerregisteret.profilering
 
+import no.nav.paw.config.hoplite.loadNaisOrLocalConfiguration
+import no.nav.paw.config.kafka.KAFKA_CONFIG_WITH_SCHEME_REG
+import no.nav.paw.config.kafka.KafkaConfig
+import org.slf4j.LoggerFactory
+
 fun main() {
-    println("${ApplicationInfo.name}, versjon: ${ApplicationInfo.version}")
+    val logger = LoggerFactory.getLogger("main")
+    val kafkaConfig = loadNaisOrLocalConfiguration<KafkaConfig>(KAFKA_CONFIG_WITH_SCHEME_REG)
+
+
 }
+
 object ApplicationInfo {
     private val pkg = this::class.java.`package`
     val version: String? = pkg.implementationVersion
